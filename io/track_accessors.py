33cdcee0cba610d8e0bb6e01512f3124c37d6059
mjens <marvin.jens@mdc-berlin.de>
2016-03-07 16:18:30 +0100

    * adding input/output and track_accessors

---
+++ io/track_accessors.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from byo.track import Accessor
+
+from byo.io.array_accessor import ArrayAccessor
+from byo.io.tsv_accessor import TSVAccessor
+from byo.io.genome_accessor import GenomeAccessor
+from byo.io.bam_accessor import BAMAccessor
+from byo.io.motifscore_accessor import MotifScoringAccessor
+
+from byo.annotation import AnnotationAccessor
+   
