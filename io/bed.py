cce83480ec5ee9c89b4e3f22fae54bd66e4fecc6
Marvin Jens <mjens@mit.edu>
2016-06-30 12:00:56 -0400

    * replaced ancient ref to sequence_data with byo

--- io/bed.py
+++ io/bed.py
@@ -1,4 +1,4 @@
-from sequence_data.io.lazytables import NamedTupleImporter
+from byo.io.lazytables import NamedTupleImporter
 
 def bed_importer(src,**kwargs):
     bed6_default = "#chrom:str\tstart:int\tend:int\tname:str\tscore:float\tstrand:str"
