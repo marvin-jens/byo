122f9a88a31f290f7b1e408cc3394a9f1a3beee2
Marvin Jens <mjens@mit.edu>
2019-02-22 13:48:37 -0500

    * support self.system as a string. Add raw-read object support to BAMAccessor

--- io/bam_accessor.py
+++ io/bam_accessor.py
@@ -20,9 +20,15 @@ def is_uniq(r):
         # if alternative alignment and reported (best) alignment have different score,
         # the mapping is unique
         return abs(XS-AS) != 0
-  
+
+def sense_of_read(read):
+    if read.is_reverse:
+        return "-"
+    else:
+        return "+"
+
 class BAMAccessor(Accessor):
-    def __init__(self,path,chrom,sense,sense_specific=False,dtype=numpy.uint32,mode="c",filter=None,dim=1,mult=True,unique=True,system='hg19',read_count=False,only_starts=False,**kwargs):
+    def __init__(self,path,chrom,sense,sense_specific=False,dtype=numpy.uint32,mode="c",filter=None,dim=1,mult=True,unique=True, system='hg19', read_count=False, full_reads=False, only_starts=False,**kwargs):
         debug("# BAMAccessor: Loading '%s' coverage for chromosome %s%s from '%s' (sense_specific=%s)" % (str(dtype),chrom,sense,path,sense_specific))
         super(BAMAccessor,self).__init__(path,chrom,sense,sense_specific=sense_specific,system=system,**kwargs)
         self.dtype=dtype
@@ -34,8 +40,9 @@ class BAMAccessor(Accessor):
         self.mult = mult
         self.unique = unique
         self.filter = filter
-        import byo.systems
-        self.system = getattr(byo.systems,system)
+        # import byo.systems
+        # self.system = getattr(byo.systems,system)
+        self.system = system
 
         import pysam
         try:
@@ -52,6 +59,10 @@ class BAMAccessor(Accessor):
             
         if read_count:
             self.get_sum = self.get_read_count
+
+        if full_reads:
+            self.get_data = self.get_raw
+            self.get_sum = self.get_sum_dummy
 
         # register for all chroms/strands
         self.covered_strands = "*"
@@ -103,16 +114,25 @@ class BAMAccessor(Accessor):
             count += w
 
         return count
+
+    def get_raw(self, chrom, start, end, sense, **kwargs):
+        pile = []
+        for read in self.bam.fetch(chrom,max(0,start),end):
+            #print read,sense_of_read(read)
+            if self.unique:
+                if not is_uniq(read):
+                    continue
+            if self.sense_specific and (sense_of_read(read) != sense): continue
+            if self.filter and not self.filter(read):
+                continue
+
+            pile.append(read)
         
+        return pile
+
     def get_data(self,chrom,start,end,sense,**kwargs):
         #debug("get_data %s:%d-%d" % (chrom,start,end))
         buf = self.get_dummy(chrom,start,end,sense)
-
-        def sense_of_read(read):
-            if read.is_reverse:
-                return "-"
-            else:
-                return "+"
 
         #print "<%s>.get_data(%s,%d,%d,%s)" % (self.path,self.chrom,start,end,sense)
         for read in self.bam.fetch(chrom,max(0,start),end):
