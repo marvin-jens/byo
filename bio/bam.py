6e6b11c411fd8a0b8e08c1bcd0a5f9bac7d05d38
Marvin Jens <mjens@mit.edu>
2019-09-18 17:14:25 -0400

    * added simplified tracks for BigWig, BAM and BED files

---
+++ bio/bam.py
@@ -0,0 +1,94 @@
+# coding=future_fstrings
+import pysam
+import os
+import itertools
+import numpy as np
+import logging
+
+class BAMTrack(object):
+    def __init__(self, fname, mode='list'):
+        self.fname = fname
+        self.bam = pysam.AlignmentFile(self.fname)
+        self.mode = mode
+        self.logger = logging.getLogger(f'byo.BAMTrack({fname})')
+        if not os.path.exists(self.fname.rstrip() + '.bai'):
+            self.logger.info("indexing BAM file...")
+            pysam.samtools.index(fname)
+
+        get_functions = {
+            'list' : (self._get_list, self._get_list_oriented),
+            '5p_array' : (self._get_5p, self._get_5p_oriented),
+        }
+
+        self.get, self.get_oriented = get_functions[mode]
+        self.logger.debug("initialized track in '{mode}' mode")
+
+    def _get(self, chrom, start, end, strand, mate=None):
+        def strand_filter(read):
+            if strand == '*':
+                return True
+            
+            rev = read.is_reverse
+            if strand == '+' and not rev:
+                return True
+                
+            elif strand == '-' and rev:
+                return True
+
+            else:
+                return False
+
+        def mate_filter(read):
+            if mate is None:
+                return True
+            
+            m1 = read.is_read1
+            m2 = read.is_read2
+            if mate == 1 and m1:
+                return True
+            
+            elif mate == 2 and m2:
+                return True
+            
+            else:
+                return False
+
+        reads = []
+        for read in self.bam.fetch(chrom, start, end):
+            if not strand_filter(read):
+                continue
+
+            if not mate_filter(read):
+                continue
+            
+            reads.append(read)
+        
+        return reads
+
+    def _get_list(self, chrom, start, end, strand, **kw):
+        reads = self._get(chrom, start, end, strand, **kw)
+        return sorted(reads, key=lambda r : r.pos)
+
+    def _get_list_oriented(self, chrom, start, end, strand, **kw):
+        reads = self._get(chrom, start, end, strand, **kw)
+        if strand == '-':
+            return sorted(reads, key=lambda r : r.aend)
+        else:
+            return sorted(reads, key=lambda r : r.pos)
+
+    def _get_5p(self, chrom, start, end, strand, **kw):
+        reads = self._get(chrom, start, end, strand, **kw)
+        res = np.zeros(end-start)
+        for r in reads:
+            x = r.pos if strand == '+' else r.aend
+            if start <= x < end:
+                res[x-start] += 1
+        
+        return res
+
+    def _get_5p_oriented(self, chrom, start, end, strand, **kw):
+        res = self._get_5p(chrom, start, end, strand, **kw)
+        if strand == '-':
+            res = res[::-1]
+        
+        return res
--- bio/bed.py
+++ bio/bed.py
@@ -1,8 +1,98 @@
+# coding=future_fstrings
 from byo.bio.lazytables import NamedTupleImporter
+import numpy as np
+import logging
+import time
+from collections import defaultdict
 
-def bed_importer(src,**kwargs):
-    bed6_default = "#chrom:str\tstart:int\tend:int\tname:str\tscore:float\tstrand:str"
+bed6_format = "#chrom:str\tstart:int\tend:int\tname:str\tscore:float\tstrand:str"
+bed12_format = "#chrom:str\tstart:int\tend:int\tname:str\tscore:float\tstrand:str\tcds_start:int\tcds_end:int\trgb:intlist\tblocks:int\tblock_sizes:intlist\tblock_starts:intlist"
 
-    for rec in NamedTupleImporter(src,descr=bed6_default,parse_comments=True,default_cast='str',keep_line=True,**kwargs):
+def bed_importer(src, fmt=bed6_format, **kwargs):
+    for rec in NamedTupleImporter(src, descr=fmt,parse_comments=False,default_cast='str',keep_line=False,**kwargs):
         yield rec
     
+
+def transcripts_from_bed(src, system=None):
+    from byo.gene_model import Transcript
+    for bed in src:
+        exon_starts = np.array(bed.block_starts) + bed.start
+        exon_ends = exon_starts + np.array(bed.block_sizes)
+        tx = Transcript(
+                bed.name,
+                bed.chrom,
+                bed.strand,
+                exon_starts,
+                exon_ends,
+                (bed.cds_start, bed.cds_end),
+                score=bed.score,
+                system=system,
+                rgb=bed.rgb
+        )
+        yield tx
+
+
+class BEDTrack(object):
+    def __init__(self, fname):
+        self.fname = fname
+        self.logger = logging.getLogger(f'byo.bio.BedTrack({fname})')
+        import gzip
+        if fname.endswith('.gz'):
+            self.bfile = gzip.GzipFile(fname, "r")
+            self.logger.debug('Gzip compression detected')
+        else:
+            self.bfile = file(fname, "r")
+
+        self.starts = defaultdict(list)
+        self.ends = defaultdict(list)
+        self.ids = defaultdict(list)
+        self.bed_records = []
+        self.ncls = {}
+
+        self._load_bed()
+        self._build_ncls()
+    
+    def _load_bed(self):
+        for line in self.bfile:
+            chrom, start, end, name, score, strand = line.rstrip().split('\t')[:6]
+            start = int(start)
+            end = int(end)
+            
+            n = len(self.bed_records)
+            self.bed_records.append( (chrom, start, end, name, score, strand) )
+            strand = chrom+strand
+
+            self.starts[strand].append(start)
+            self.ends[strand].append(end)
+            self.ids[strand].append(n)
+
+        self.logger.debug('loaded {} BED records'.format(len(self.bed_records)))
+
+    def _build_ncls(self):
+        import ncls
+        t0 = time.time()
+        for strand in sorted(self.ids.keys()):
+            starts = np.array(self.starts[strand])
+            ends = np.array(self.ends[strand])
+            ids = np.array(self.ids[strand])
+            # print strand, starts, ends, ids
+            self.ncls[strand] = ncls.NCLS(
+                starts,
+                ends,
+                ids,
+            )
+
+        self.bed_records = np.array(self.bed_records, dtype=object)
+        dt = time.time() - t0
+        self.logger.debug("built NCLists for {0} items in {1:.3f}seconds".format(len(self.bed_records), dt))
+    
+    def get(self, chrom, start, end, sense):
+        strand = chrom+sense
+        ncls = self.ncls[strand]
+        found = [self.bed_records[fid[2]] for fid in self.ncls[strand].find_overlap(start, end)]
+
+        return found
+
+    def get_oriented(self, chrom, start, end, sense):
+        return self.get(chrom, start, end, sense)
+
