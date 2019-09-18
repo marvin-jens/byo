9eed734d2f4e8cf82446de74b69a90c39482882f
Marvin Jens <mjens@mit.edu>
2019-09-18 17:40:48 -0400

    * small error

--- bio/bam.py
+++ bio/bam.py
@@ -17,7 +17,10 @@ class BAMTrack(object):
 
         get_functions = {
             'list' : (self._get_list, self._get_list_oriented),
-            '5p_array' : (self._get_5p, self._get_5p_oriented),
+            '5p_array' : (self._get_5p, self._get_oriented),
+            '3p_array' : (self._get_3p, self._get_oriented),
+            'coverage' : (self._get_cov, self._get_oriented),
+            'count' : (self._get_count, self._get_count),
         }
 
         self.get, self.get_oriented = get_functions[mode]
@@ -65,6 +68,10 @@ class BAMTrack(object):
         
         return reads
 
+    def _get_count(self, *argc, **kw):
+        reads = self._get(*argc, **kw)
+        return len(reads)
+
     def _get_list(self, chrom, start, end, strand, **kw):
         reads = self._get(chrom, start, end, strand, **kw)
         return sorted(reads, key=lambda r : r.pos)
@@ -86,8 +93,29 @@ class BAMTrack(object):
         
         return res
 
-    def _get_5p_oriented(self, chrom, start, end, strand, **kw):
-        res = self._get_5p(chrom, start, end, strand, **kw)
+    def _get_3p(self, chrom, start, end, strand, **kw):
+        reads = self._get(chrom, start, end, strand, **kw)
+        res = np.zeros(end-start)
+        for r in reads:
+            x = r.pos if strand == '-' else r.aend
+            if start <= x < end:
+                res[x-start] += 1
+        
+        return res
+
+    def _get_cov(self, chrom, start, end, strand, **kw):
+        reads = self._get(chrom, start, end, strand, **kw)
+        res = np.zeros(end-start)
+        for r in reads:
+            x, y = r.pos, r.aend
+            s = max(0, x - start)
+            e = min(end - start, y - start)
+            res[s:e] += 1
+
+        return res
+
+    def _get_oriented(self, chrom, start, end, strand, **kw):
+        res = self.get(chrom, start, end, strand, **kw)
         if strand == '-':
             res = res[::-1]
         
