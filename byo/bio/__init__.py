140df8fdc0fd120e04e80246d90584e291036435
Marvin Jens <marvin.jens@mdc-berlin.de>
2021-09-20 14:59:59 +0200

    * black formatting

--- byo/bio/__init__.py
+++ byo/bio/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
-def ensure_path(path,silent=False):
+
+
+def ensure_path(path, silent=False):
     path = os.path.abspath(os.path.expandvars(path))
     if not silent and (not os.path.isdir(path) and path):
         try:
@@ -10,37 +12,46 @@ def ensure_path(path,silent=False):
             pass
     return path
 
+
 def stat_chunks(src):
-    #print src
+    # print src
     from numpy import array
     import re
+
     class Item(object):
-        def __init__(self,title,col_labels,row_labels,data,local_options):
+        def __init__(self, title, col_labels, row_labels, data, local_options):
             self.title = title
-            #self.name = name
+            # self.name = name
             self.col_labels = col_labels
             self.row_labels = row_labels
             self.data = array(data)
             self.local_options = local_options
 
             from string import maketrans
-            table = maketrans(" +-:","_pm_")
+
+            table = maketrans(" +-:", "_pm_")
             self.safe_title = title.translate(table)
-            for rl,d in zip(row_labels,data):
+            for rl, d in zip(row_labels, data):
                 rl = rl.translate(table)
                 if len(d) == 1:
                     d = d[0]
-                setattr(self,rl,d)
-                
+                setattr(self, rl, d)
+
         def __repr__(self):
-            attr_str = ", ".join(["%s=%s" % (k,str(v)) for k,v in self.__dict__.items() if not k.startswith("__")])
-            return "[Item: %s {%s}]" % (self.title,attr_str)
+            attr_str = ", ".join(
+                [
+                    "%s=%s" % (k, str(v))
+                    for k, v in self.__dict__.items()
+                    if not k.startswith("__")
+                ]
+            )
+            return "[Item: %s {%s}]" % (self.title, attr_str)
 
-            #print self.title, self.safe_title
+            # print self.title, self.safe_title
 
-        def __getattr__(self,attr):
+        def __getattr__(self, attr):
             try:
-                return super(Item,self).__getattr__(attr)
+                return super(Item, self).__getattr__(attr)
             except AttributeError:
                 return "N/A"
 
@@ -53,7 +64,7 @@ def stat_chunks(src):
     for line in src:
         if line.startswith(">"):
             if data:
-                yield Item(title,col_labels,row_labels,data,local_options)
+                yield Item(title, col_labels, row_labels, data, local_options)
                 data = []
                 row_labels = []
                 local_options = []
@@ -62,7 +73,7 @@ def stat_chunks(src):
             continue
 
         if line.startswith("##"):
-            local_options = re.split("\s+",line[2:].rstrip())
+            local_options = re.split("\s+", line[2:].rstrip())
             continue
 
         if line.startswith("#"):
@@ -76,46 +87,54 @@ def stat_chunks(src):
             data.append([float(p) for p in parts[1:]])
 
     if data:
-        yield Item(title,col_labels,row_labels,data,local_options)
+        yield Item(title, col_labels, row_labels, data, local_options)
         data = []
         row_labels = []
         local_options = []
-    
+
 
 def qfa_chunks(lines):
     """
     Iterates over FASTQ text lines from a file like object and yields
-    NamedTuple instances of the FASTQ with attributes 
+    NamedTuple instances of the FASTQ with attributes
     qfa.name, qfa.seq, qfa.qual
     """
     from collections import namedtuple
-    QFA = namedtuple("qfa_tuple","name,seq,qual")
-    
+
+    QFA = namedtuple("qfa_tuple", "name,seq,qual")
+
     I = lines.__iter__()
 
     try:
         while I:
-            name = I.next().rstrip()[1:]
-            seq = I.next().rstrip()
-            plus = I.next().rstrip()[1:]
-            qual = I.next().rstrip()
-        
-            yield QFA(name,seq,qual)
+            # name = I.next().rstrip()[1:]
+            # seq = I.next().rstrip()
+            # plus = I.next().rstrip()[1:]
+            # qual = I.next().rstrip()
+            name = next(I).rstrip()[1:]
+            seq = next(I).rstrip()
+            plus = next(I).rstrip()[1:]
+            qual = next(I).rstrip()
+
+            yield QFA(name, seq, qual)
     except StopIteration:
         pass
 
+
 fastq_chunks = qfa_chunks
 
-def fasta_chunks(lines,strip=True,fuse=True):
+
+def fasta_chunks(lines, strip=True, fuse=True):
     chunk = ""
     data = []
 
     for l in lines:
-        if l.startswith("#"): continue
+        if l.startswith("#"):
+            continue
         if l.startswith(">"):
             if data and chunk:
-                #print chunk
-                yield chunk,"".join(data)
+                # print chunk
+                yield chunk, "".join(data)
 
                 if strip:
                     data = []
@@ -130,34 +149,41 @@ def fasta_chunks(lines,strip=True,fuse=True):
                 data.append(l)
 
     if data and chunk:
-        yield chunk,"".join(data)
+        yield chunk, "".join(data)
+
 
 from byo import k_mers
-from numpy import array,uint8
+from numpy import array, uint8
 
 nums = {}
-for i,b in enumerate(k_mers(1)):
+for i, b in enumerate(k_mers(1)):
     nums[b] = i
 
+
 def numeric(seq):
-    seq = seq.lower().replace("n","a")
-    return array([nums[s] for s in seq],dtype=uint8)
+    seq = seq.lower().replace("n", "a")
+    return array([nums[s] for s in seq], dtype=uint8)
+
 
 def fasta_as_nums(fname):
-    #logger.debug("scan_fasta('%s')" % fname)
-    #bases = {}
-        #bases[i] = b
+    # logger.debug("scan_fasta('%s')" % fname)
+    # bases = {}
+    # bases[i] = b
 
-    for fa_id,seq in fasta_chunks(file(fname)):
-        #if 'n' in seq: continue
-        yield fa_id,numeric(seq)
+    for fa_id, seq in fasta_chunks(file(fname)):
+        # if 'n' in seq: continue
+        yield fa_id, numeric(seq)
 
-   #logger.debug("read %d sequences of length %d" % (N,L))
+
+# logger.debug("read %d sequences of length %d" % (N,L))
 
 
 import re
+
+
 def wigfix_chunks(lines):
     import numpy
+
     regexp = r"^fixedStep\s+chrom=(?P<chrom>\w+)\s+start=(?P<start>\d+)\s+step=(?P<step>\d+).*"
 
     chrom = "none"
@@ -165,31 +191,30 @@ def wigfix_chunks(lines):
     step = 1
     linebuf = []
 
-    def emit_block(linebuf,step,start):
-        scores = numpy.array(linebuf,dtype=numpy.float32).repeat(step)
-        return chrom,scores,start-1,start-1+len(scores)
+    def emit_block(linebuf, step, start):
+        scores = numpy.array(linebuf, dtype=numpy.float32).repeat(step)
+        return chrom, scores, start - 1, start - 1 + len(scores)
 
     for l in lines:
         l = l.strip()
-        if not l or l.startswith('track'):
+        if not l or l.startswith("track"):
             continue
 
         if l.startswith("f"):
-            info = re.match(regexp,l)
+            info = re.match(regexp, l)
 
             if linebuf:
-                yield emit_block(linebuf,step,start)
+                yield emit_block(linebuf, step, start)
                 linebuf = []
 
             d = info.groupdict()
-            chrom = d['chrom']
-            start = int(d['start'])
-            step = int(d['step'])
+            chrom = d["chrom"]
+            start = int(d["start"])
+            step = int(d["step"])
 
         else:
             linebuf.append(l)
 
     if linebuf:
-        yield emit_block(linebuf,step,start)
+        yield emit_block(linebuf, step, start)
         linebuf = []
-
